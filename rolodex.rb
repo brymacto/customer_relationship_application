1de44d5fcb31901a25e7662559568d2c4458e76b
Bryan <brymac@gmail.com>
2015-06-12 16:17:01 -0400

    Modify first name is now working

--- rolodex.rb
+++ rolodex.rb
@@ -15,8 +15,9 @@ class Rolodex
 
   def modify_contact(contact_id)
     contact_arr_index = find_contact_index(contact_id)
-    puts @contacts[contact_arr_index].display_all_values
-    puts "What do you wish to edit?"
+    current_contact = @contacts[contact_arr_index]
+    current_contact.display_all_values
+    puts "What do you wish to edit for record #{current_contact.id}?"
     puts "[1] First name"
     puts "[2] Last name"
     puts "[3] Email"
@@ -27,6 +28,10 @@ class Rolodex
     case field_edit
     when 1
       puts "The first name is currently #{@contacts[contact_arr_index].first_name}"
+      print "Please enter an updated first name: "
+      first_name_update = gets.chomp
+      current_contact.first_name = first_name_update
+
     when 2
       puts "Editing last name"
     when 3
@@ -38,8 +43,12 @@ class Rolodex
     else
       puts "Invalid entry.  Cancelling."
     end
+    modify_status_message(current_contact)
   end
-
+  def modify_status_message(record)
+    puts "The record #{record.id} is now:"
+    record.display_all_values
+  end
   def find_contact_index(id)
     @contacts.index do |contact|
       contact.id.to_i == id.to_i
